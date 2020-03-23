c09c3dc18a7a32a1c9fca03f46dcfe0b25f9c819
Luis Cuende <me@luisivan.net>
2020-03-23 12:50:21 +0000

    Fixed couple typos and added Material UI as dependency

--- squads/src/squads/NewSquad.js
+++ squads/src/squads/NewSquad.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 
-import './Styles.css';
+import './styles.css';
 
 const useStyles = makeStyles(theme => ({
 	root: {
--- squads/src/squads/ZZZ.js
+++ squads/src/squads/ZZZ.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './Styles.css';
+import './styles.css';
 
 const ZZZ = () => {
 	return (
