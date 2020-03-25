29bd7f11066a58cd96d48dc832b0762df6bf9da5
w0xter <luispozogilo@gmail.com>
2020-03-25 19:05:00 +0100

    preparing merge

--- squads/src/components/new/NewSquad.js
+++ squads/src/components/new/NewSquad.js
@@ -8,6 +8,11 @@ import '../new/Register';
 import '../styles/styles.css';
 import Register from '../new/Register';
 
+var name;
+var description;
+//var dao;
+var inviteLink;
+
 class NewSquad extends React.Component {
 	constructor() {
 		super();
@@ -25,23 +30,32 @@ class NewSquad extends React.Component {
 
 				<div className='container'>
 					<section>
-						<h3>&#9937; Launch a New Squad &#9937;</h3>
+						<h3> <span>&#9937;</span> Launch a New Squad <span>&#9937;</span></h3>
 						<form id='form' autoComplete='off'>
 							<TextField
 								id='standard-basic'
 								label='Squad Name'
 								required
+								onChange={event => {
+									name = event.target.value;
+								}}
 							/>
 							<TextField
 								id='standard-basic'
 								label='Squad Description'
 								required
+								onChange={event => {
+									description = event.target.value;
+								}}
 							/>
 
 							<TextField
 								id='standard-basic'
 								label='Chat Invite Link'
 								required
+								onChange={event => {
+									inviteLink = event.target.value;
+								}}
 							/>
 							<hr />
 						</form>
@@ -75,12 +89,13 @@ class NewSquad extends React.Component {
 						</a>
 					</section>
 					<section id='init-squad'>
-						<h3>&#128640; TakeOff &#128640;</h3>
+						<h3> <span>&#128640;</span> TakeOff <span>&#128640;</span></h3>
 						<Button
 							id='next'
 							variant='outlined'
 							color='secondary'
 							disabled={this.state.disabled}
+							onClick={this.submitSquad}
 						>
 							Init DAO
 						</Button>
