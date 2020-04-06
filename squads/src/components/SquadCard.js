1dea81515238f282a965c93ef115202b382b997d
Luis Cuende <me@luisivan.net>
2020-04-06 18:37:53 +0100

    Using PageHeader in SquadDetail

--- squads/src/components/SquadCard.js
+++ squads/src/components/SquadCard.js
@@ -9,7 +9,7 @@ export default function SquadCard({ name, desc, url, verified }) {
       actions={[
         <Link to={url}><Button type="primary">Read more →</Button></Link>,
       ]}
-      title={<>{name}{verified ? <Tag color="#52c41a" style={{ float: 'right' }}>Verified</Tag> : ''}</>}
+      title={<>{name}{verified ? <Tag color="green" style={{ float: 'right' }}>Verified</Tag> : ''}</>}
     >
       <div style={{ height: 164 }}>
         <ReactMarkdown source={desc.substring(0, 180).replace(/[\#]*/, '')}></ReactMarkdown>
--- squads/src/components/SquadDetail.js
+++ squads/src/components/SquadDetail.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import ReactMarkdown from 'react-markdown'
 import { squadDetails } from "../requests";
-import { Statistic, Row, Col, Button } from 'antd';
+import { Statistic, Row, Col, Button, PageHeader, Tag } from 'antd';
 
 const SquadDetails = props => {
   const { squadId } = props.match.params;
@@ -12,8 +12,6 @@ const SquadDetails = props => {
     async function getDetails() {
       try {
         const result = await squadDetails(squadId);
-        console.log("Result Squad Details: ");
-        console.log(result);
         if (result.data && result.data.squad) {
           setDetails(result.data.squad);
           setDonation(
@@ -21,7 +19,7 @@ const SquadDetails = props => {
           );
         }
       } catch (err) {
-        console.log(err);
+        console.log(err)
       }
     }
     getDetails();
@@ -34,16 +32,27 @@ const SquadDetails = props => {
 
   return (
     <div style={{ marginTop: 32, marginBottom: 32 }}>
-      <h1>{details.name}</h1>
-      <ReactMarkdown source={details.description}></ReactMarkdown>
-      <Row>
+      <PageHeader
+        onBack={() => window.history.back()}
+        title={details.name}
+        tags={details.verified ? <Tag color="green">Verified</Tag> : ''}
+        extra={[
+          <Button key="1" type="primary">
+            Donate
+          </Button>,
+        ]}
+        style={{ padding: 0 }}
+      ></PageHeader>
+
+      <Row style={{ marginTop: 32, marginBottom: 32 }}>
         <Col span={12}>
-          <Statistic title="Total donated" value={112893} />
+          <Statistic title="Total donated" prefix="$" value={112893} precision={2} valueStyle={{ color: '#3f8600' }} />
         </Col>
         <Col span={12}>
-          <Statistic title="Remaining" value={112893} precision={2} />
+          <Statistic title="Remaining" prefix="$" value={112893} precision={2} />
         </Col>
       </Row>
+      <ReactMarkdown source={details.description}></ReactMarkdown>
       <Row>
         <Button type="primary" href={`https://buy.ramp.network?swapAsset=DAI&userAddress=${details.daoAddress}`}>Donate with Ramp (EU)</Button>
         <Button type="primary" href={"https://pay.sendwyre.com/purchase?destCurrency=DAI&paymentMethod=debit-card&dest=" + details.daoAddress + "&redirectUrl=http://localhost:3000/squad/" + details._id}>Donate with Wyre (US)</Button>
