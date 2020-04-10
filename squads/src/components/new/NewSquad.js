56daeba2039be30fb5247584ea1f0b4ca73e1c31
Luis Cuende <me@luisivan.net>
2020-04-10 18:02:47 +0100

    Using the Form component for form validation

--- squads/src/components/new/NewSquad.js
+++ squads/src/components/new/NewSquad.js
@@ -1,25 +1,24 @@
 import React, { useState } from "react";
-import { Steps, Button, message, Form, Input, Row, Col, Typography } from "antd";
+import {
+  Steps,
+  Button,
+  message,
+  Row,
+  Col,
+  Typography,
+} from "antd";
 import Register from "./Register";
+import SquadForm from "./SquadForm";
 import { findSquad, createSquad } from "../../requests";
 import write from "../../assets/write.svg";
 
 const { Step } = Steps;
-const { TextArea } = Input;
-let inviteLinkRegex = RegExp(
-  /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?(t\.me\/|chat\.whatsapp\.com)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/,
-  "i"
-);
 
 export default function NewSquad() {
   const [current, setCurrent] = useState(0);
   const [error, setError] = useState("");
-  const [name, setName] = useState("");
-  const [inviteLink, setInviteLink] = useState("");
-  const [description, setDescription] = useState("");
-  const [visible, setVisible] = useState(false);
-  const [formValid, setFormValid] = useState(false);
   const [walletConnected, setWalletConnected] = useState(false);
+  const [details, setDetails] = useState({});
 
   //STEPS CONTROLLERS
   const next = () => {
@@ -32,80 +31,6 @@ export default function NewSquad() {
     setCurrent(val);
   };
 
-  //FORM VALIDATION
-
-  const checkName = () => {
-    return new Promise((resolve, reject) => {
-      findSquad({ name: name })
-        .then((response) => {
-          console.log("Valid Name?");
-          console.log(response);
-          if (response.data.squads.length === 0) {
-            resolve();
-          } else {
-            message.error(
-              "This name is in use, you need to choose a different Squad Name"
-            );
-            reject();
-          }
-        })
-        .catch((err) => {
-          message.error("Somenthing goes wrong. Try it again in a while");
-          reject();
-        });
-    });
-  };
-  const valideInviteLink = () => {
-    return new Promise((resolve, reject) => {
-      if (inviteLink.length === 0) {
-        message.error("You have to provide an invitation link.");
-        reject();
-      } else if (!inviteLinkRegex.test(inviteLink)) {
-        message.error("You have to provide a valid invitation link.");
-        reject();
-      } else resolve();
-    });
-  };
-  const validateName = () => {
-    return new Promise((resolve, reject) => {
-      if (name.length === 0) {
-        message.error("You have to provide Squad name");
-        reject();
-      } else resolve();
-    });
-  };
-  const validateDescription = () => {
-    return new Promise((resolve, reject) => {
-      if (description.length === 0) {
-        message.error("You have to provide a description");
-        reject();
-      } else resolve();
-    });
-  };
-  const validateForm = () => {
-    validateName()
-      .then((data) => {
-        return checkName();
-      })
-      .then((data) => {
-        return valideInviteLink();
-      })
-      .then((data) => {
-        return validateDescription();
-      })
-      .then((data) => {
-        message.success("Everything is correct!");
-        setError("");
-        setFormValid(true);
-        next();
-      })
-      .catch((err) => {
-        setError("error");
-        setFormValid(false);
-        console.log(err);
-      });
-  };
-  //STEPS VALIDATION CONTROLL
   const validateStep = () => {
     if (current === 0) {
       Promise.resolve(localStorage.getItem("fortmatic")).then((fortmatic) => {
@@ -121,14 +46,13 @@ export default function NewSquad() {
         }
       });
     } else if (current === 1) {
-      validateForm();
     } else if (current === 2) {
-      if (formValid !== false && walletConnected !== false) {
+      if (walletConnected === true) {
         let dao = "0x0000000000000000ABADBABE0000000000000000";
         let data = {
-          name: name,
-          description: description,
-          inviteLink: inviteLink,
+          name: details.name,
+          description: details.description,
+          inviteLink: details.inviteLink,
           daoAddress: dao,
         };
         createSquad(data)
@@ -147,6 +71,11 @@ export default function NewSquad() {
     }
   };
 
+  const setSquadDetails = (values) => {
+    setDetails(values);
+    next();
+  }
+
   const steps = [
     {
       title: "Register",
@@ -157,31 +86,10 @@ export default function NewSquad() {
       content: (
         <Row gutter={64} justify="space-around" align="middle">
           <Col xs={24} sm={24} md={12}>
-            <label>Squad Name</label>
-            <Input
-              name="name"
-              placeholder="Name"
-              value={name}
-              onChange={(evt) => setName(evt.target.value)}
-            />
-            <label>Squad Invitation Link</label>
-            <Input
-              name="inviteLink"
-              placeholder="InviteLink"
-              value={inviteLink}
-              onChange={(evt) => setInviteLink(evt.target.value)}
-            />
-            <label>Squad Description</label>
-            <TextArea
-              rows={20}
-              name="description"
-              placeholder="Description"
-              value={description}
-              onChange={(evt) => setDescription(evt.target.value)}
-            />
+            <SquadForm onFinish={setSquadDetails}></SquadForm>
           </Col>
           <Col xs={0} sm={0} md={12}>
-            <img src={write} style={{ width: '100%' }} />
+            <img src={write} style={{ width: "100%" }} />
           </Col>
         </Row>
       ),
@@ -195,26 +103,20 @@ export default function NewSquad() {
   localStorage.setItem("fortmatic", "");
   return (
     <>
-      <Steps current={current} status={error} style={{ marginTop: 32, marginBottom: 32 }}>
+      <Steps
+        current={current}
+        status={error}
+        style={{ marginTop: 32, marginBottom: 32 }}
+      >
         {steps.map((item) => (
           <Step key={item.title} title={item.title} />
         ))}
       </Steps>
       <div className="steps-content">{steps[current].content}</div>
       <div className="steps-action">
-        {current < steps.length - 1 && (
-          <Button type="primary" onClick={() => validateStep()}>
-            Next
-          </Button>
-        )}
         {current === steps.length - 1 && (
           <Button type="primary" onClick={() => validateStep()}>
-            Launch {name} Squad!
-          </Button>
-        )}
-        {current > 0 && (
-          <Button style={{ margin: 8 }} onClick={() => prev()}>
-            Previous
+            Launch {details.name} Squad!
           </Button>
         )}
       </div>
