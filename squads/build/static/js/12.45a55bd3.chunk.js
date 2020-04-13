764e5a14963e57c9c318879a00a78e4a930cc01c
w0xter <luispozogilo@gmail.com>
2020-04-13 19:52:01 +0200

    Checking if the name is alredy in use before go to next step

--- squads/build/static/js/12.45a55bd3.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpsquads=this.webpackJsonpsquads||[]).push([[12,13],{256:function(n,s){},257:function(n,s){},260:function(n,s){},261:function(n,s){}}]);
-//# sourceMappingURL=12.45a55bd3.chunk.js.map
