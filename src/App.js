26659e6effd295d05a5b1b6c315469a2164b9cae
Pacien Boisson <pakokrew@gmail.com>
2020-04-29 12:42:06 +0200

    Fix router switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import NewSquad from "./components/new/NewSquad";
 import Register from "./components/new/Register";
 import SquadPage from "./pages/SquadPage";
@@ -13,21 +13,26 @@ import 'antd/dist/antd.less';
 function App() {
   return (
     <Router>
-      <Switch>
-        <Layout>
+      <Layout>
+        <Switch>
           <Route exact path="/">
             <Hero />
             <SquadList />
           </Route>
-          <Route exact path="/new">
+          <Route path="/new">
             <NewSquad />
           </Route>
-          <Route exact path="/new/register">
+          <Route path="/new/register">
             <Register />
           </Route>
-          <Route path="/squad/:squadId" component={SquadPage} />
-        </Layout>
-      </Switch>
+          <Route path="/squad/:squadId">
+            <SquadPage />
+          </Route>
+          <Route>
+            <Redirect to="/"/>
+          </Route>
+        </Switch>
+      </Layout>
     </Router>
   );
 }
