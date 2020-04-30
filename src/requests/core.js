4af189af9260bf2eea55ca58f43055a3b921d4af
Pacien Boisson <pakokrew@gmail.com>
2020-04-30 15:15:19 +0200

    Update backend address

--- src/requests/core.js
+++ src/requests/core.js
@@ -1,5 +1,3 @@
-//import axios from 'axios'
-// export const API = process.env.API_URL || "http://localhost:3001";
-export const API = process.env.API_URL || "http://ec2-3-19-74-56.us-east-2.compute.amazonaws.com:3001";
+export const API = process.env.API_URL || "https://testbackend.helpdao.org";
 
 
